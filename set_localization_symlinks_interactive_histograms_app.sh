43db8d1b1efc0251d3a01f9f77b59dfd7bf961ed
Nicolas Audibert <104156050+nicolasaudibert@users.noreply.github.com>
2024-02-22 15:06:30 +0100

    Delete set_localization_symlinks_interactive_histograms_app.sh

--- set_localization_symlinks_interactive_histograms_app.sh
+++
@@ -1,9 +0,0 @@
-# Usage: set_localization_symlinks_interactive_histograms_app.sh targetLanguage
-
-# Delete existing symlinks
-rm interactive_histogram_display.html
-rm interactive_histogram_display.labels.txt
-
-# Create new symlinks
-ln -s "interactive_histogram_display.${1}.html" "interactive_histogram_display.html"
-ln -s "interactive_histogram_display.labels.${1}.txt" "interactive_histogram_display.labels.txt"
